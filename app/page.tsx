3e7e21590ac62540a7989f74343a12ce1cabe2d2
Andriy Pashynnyk <tobirawork@gmail.com>
2024-04-06 16:59:10 +0300

    add drizzle

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main>
       <Button>click</Button>
