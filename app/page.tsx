66d477c0b44e8769036548bff6e53c99aac2ce3e
Andriy Pashynnyk <tobirawork@gmail.com>
2024-04-07 16:04:28 +0300

    remove caching of the index route

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { NotesList, NotesListSkeleton } from "@/components/notes-list";
 import { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   return (
     <main>
