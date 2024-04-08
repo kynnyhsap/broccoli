07cdeaf201d4f13e0d001551cd9f01fc6fc2694e
Andriy Pashynnyk <tobirawork@gmail.com>
2024-04-08 22:01:24 +0300

    try dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { NotesList, NotesListSkeleton } from "@/components/notes-list";
 import { Suspense } from "react";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   return (
