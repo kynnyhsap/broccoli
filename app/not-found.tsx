dba997216b6f5ccf7cdcfff8ee6583cc1f861fd2
Andriy Pashynnyk <tobirawork@gmail.com>
2024-04-10 19:29:02 +0300

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-16 my-16">
+      <Image alt="confused.gif" height="300" width="300" src="/confused.gif" />
+
+      <h1 className="text-4xl font-bold">404</h1>
+
+      <Link href="/">
+        <Button variant="outline" size="lg">
+          Back to the homepage
+        </Button>
+      </Link>
+    </div>
+  );
+}
