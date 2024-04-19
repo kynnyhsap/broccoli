7e0124f343b540cb43c93195d74eb10d77098af1
Andriy Pashynnyk <tobirawork@gmail.com>
2024-04-20 00:29:03 +0300

    add placeholder pages for episode and podcast

---
+++ app/episode/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default async function EpisodePage() {
+  return (
+    <div>
+      <h1>Episode Page</h1>
+    </div>
+  );
+}
---
+++ app/podcast/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default async function PodcastPage() {
+  return (
+    <div>
+      <h1>Podcast Page</h1>
+    </div>
+  );
+}
